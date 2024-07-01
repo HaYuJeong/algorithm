342cfce37176da84fb87b8f26ec97b71f4bfd0ac
HaYuJeong <161279944+HaYuJeong@users.noreply.github.com>
2024-07-01 14:43:52 +0900

    hash 포켓몬
    코드 수정

--- src/main/java/org/hash/PocketMon.java
+++ src/main/java/org/hash/PocketMon.java
@@ -1,5 +1,8 @@
 package org.hash;
 
+import java.util.HashSet;
+import java.util.Scanner;
+
 /**
  * packageName : org.hash
  * fileName : PocketMon
@@ -15,7 +18,22 @@ package org.hash;
  */
 public class PocketMon {
     public static void main(String[] args) {
-        int nums[] = new int[0];
-        int len = nums.length;
+//        int nums[] = new int[0];    // nums = [3,2,1,3]
+        int nums[] = new int[]{3, 2, 1, 3};
+        int len = nums.length;      // len = 4
+
+        HashSet<Integer> set = new HashSet<>();     // 중복을 거르는 자료구조 HashSet
+        for (int i = 0; i < len; i++) {
+            set.add(nums[i]);
+        }
+
+
+        System.out.println(Math.min(set.size(), len/2));
+//        int setLen = (set.size() * set.size()-1) / 2 ;
+//        if ( setLen <= len/2) {
+//            System.out.println(setLen);
+//        } else {
+//            System.out.println(len/2);
+//        }
     }
 }
