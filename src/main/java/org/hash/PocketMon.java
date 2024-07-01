2e2c66942578c435fb3fcb138dbb3d44bbb81474
HaYuJeong <161279944+HaYuJeong@users.noreply.github.com>
2024-07-01 13:58:25 +0900

    hash 포켓몬
    HashSet 자료구조를 사용한 중복 제거, 배열사용

---
+++ src/main/java/org/hash/PocketMon.java
@@ -0,0 +1,21 @@
+package org.hash;
+
+/**
+ * packageName : org.hash
+ * fileName : PocketMon
+ * author : hayj6
+ * date : 2024-07-01(001)
+ * description :
+ * 요약 :
+ * <p>
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-07-01(001)         hayj6          최초 생성
+ */
+public class PocketMon {
+    public static void main(String[] args) {
+        int nums[] = new int[0];
+        int len = nums.length;
+    }
+}
